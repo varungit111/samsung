7f6df5656fb86b6ed54867fb598303cf4077982e
Varun <varun.kumar11120@gmail.com>
2026-02-21 13:38:55 +1300

    samsung

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("hello world from external javascript")
+
+const firstAndLatName= ("Varun K")
+console.log(firstAndLatName)
+
+let age= 22
+console.log(age)
+
+// Variables
+
+let currentYear= 2026
+let myBirthYear=2020
+const ageCalc=currentYear-myBirthYear;
+console.log(currentYear)
+console.log(myBirthYear)
+console.log(ageCalc)
+
